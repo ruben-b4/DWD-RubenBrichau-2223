52286ee46c66cb4d74ba08533008b5c835017ae9
ruben-b4 <ruben.brichau@student.odisee.be>
2023-04-11 01:45:44 +0200

    Project 3

--- project/website/js/scripts.js
+++ project/website/js/scripts.js
@@ -7,8 +7,10 @@ const searchInput = document.getElementById('search-input');
 const soundButton = document.querySelector('.sound__background');
 const sound = document.querySelector('.soundwrapper');
 
-const bgcolor = `rgb(${+Math.random() * 100}, ${+Math.random() * 100}, ${+Math.random() * 100})`;
+const bgcolor = `rgb(${+Math.random() * 100}, ${+Math.random() * 100}, ${+Math.random() * 100})`; // RVDL Buttons toledo
 soundButton.style.backgroundColor = bgcolor;
+
+let activeButtons = [];
 
 searchInput.addEventListener('keydown', (event) => {
     // reageren wnr enter 
@@ -26,10 +28,9 @@ async function search() {
         const response = await fetch(`https://freesound.org/apiv2/search/text/?query=${searchTerm}&token=${API_KEY}&fields=id,name,previews,duration,images`);
         const data = await response.json();
 
-        // verwijder alle knoppen en par uit sound
         sound.innerHTML = '';
 
-        // maakt knop en par
+        // maakt button en paragraph
         data.results.slice(0, NUM_RESULTS).forEach(result => {
             const button = document.createElement('button');
             const par = document.createElement('p');
@@ -37,13 +38,46 @@ async function search() {
             par.innerHTML = result.name;
             par.setAttribute('class', 'paragraphs');
             button.setAttribute('class', 'sound__button');
-            button.style.backgroundColor = bgcolor;
+            button.style.backgroundColor = bgcolor; 
 
+            let isPlaying = false;
+            let audio = null;
+            
+            // inspiratie genomen https://www.youtube.com/watch?v=xu3y6lKD6kY&t=576s
             button.addEventListener('click', () => {
-                // speelt het geluid af
-                const audio = new Audio(result.previews['preview-lq-mp3']);
-                audio.play();
+                if (activeButtons.length > 0) {
+                    // stop alle actieve audio
+                    activeButtons.forEach((activeButton) => {
+                        activeButton.audio.pause();
+                        activeButton.isPlaying = false;
+                    });
+                    activeButtons = [];
+                }
+                if (audio && isPlaying) {
+                    // stop deze audio
+                    audio.pause();
+                    isPlaying = false;
+
+                    // verwijderd deze knop uit activebuttons, inspiratie: https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array-in-javascript 
+                    const index = activeButtons.indexOf(button);
+                    if (index > -1) {
+                        activeButtons.splice(index, 1); // splice zal wegdoen active
+                    }
+                } else {
+                    // start audio
+                    isPlaying = true;
+                    audio = new Audio(result.previews['preview-lq-mp3']);
+                    audio.play();
+
+                    // voegt button toe aan activebuttons
+                    activeButtons.push({
+                        button,
+                        audio,
+                        isPlaying
+                    });
+                }
             });
+            
             button.appendChild(par);
             sound.appendChild(button);
         });
